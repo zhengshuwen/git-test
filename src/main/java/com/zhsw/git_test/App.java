3d163bb5cbd0618e91643069b22c28645aa2c9bf
zhengshuwen <563286464@qq.com>
2017-10-27 15:30:18 +0800

    git-test

---
+++ src/main/java/com/zhsw/git_test/App.java
@@ -0,0 +1,13 @@
+package com.zhsw.git_test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
